b05f6fb25b83fd6bf90f7f892faa88ec3ba69bb2
CalJersey <crippe@gmail.com>
2017-07-11 16:58:42 -0700

    fixed header css, added stoplight image and logic, added countdown (needs css fix)

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,18 @@ $(document).ready(function(){
   car1.makeCar();
   let car2 = new Car("car2","car2","car2","images/police.gif",0,0,0,116,65);
   car2.makeCar();
+
+  //startButton listener
+  $("#startButton").on("click", function(event){
+    event.preventDefault();
+    let endDate = new Date().getTime();
+    endDate = endDate + 5000;
+    countDown(5,true,true,100,endDate);
+  })
   // code in here
   $(document).on("keydown",function(event){
     event.preventDefault();
-    console.log(event.which);
+
     if (event.which == keyCode){
       car1.incrementPosRight();
     };
@@ -17,7 +25,7 @@ $(document).ready(function(){
   });
 });
 
-function Car(id,name,cssClass,image="images/police.gif", speed=0, posRight=0, posTop=0,width=116,height=65) {
+function Car(id,name,cssClass,image="images/police.gif", speed=0, posRight=0, posTop=0,width=116,height=65,actionKey) {
 	this.image = image;
 	this.speed = speed;
   this.cssClass = cssClass;
@@ -27,15 +35,14 @@ function Car(id,name,cssClass,image="images/police.gif", speed=0, posRight=0, po
   this.name = name;
   this.width = width;
   this.height = height;
+  this.actionKey = actionKey;
   this.setRight = function(posRight=this.posRight){$(`#${this.id}`).css({right:posRight})};
   //this.setTop = function(posY=posY){};
   this.increaseSpeed = function(units=1){
     this.speed += units;
   };
   this.incrementPosRight = function(increment=1){
-    console.log(`Before: ${this.posRight}`);
     this.posRight+=increment;
-    console.log(`Before: ${this.posRight}`);
     this.setRight();
   }
   this.decreaseSpeed = function(units=1){
@@ -45,5 +52,42 @@ function Car(id,name,cssClass,image="images/police.gif", speed=0, posRight=0, po
       $("#raceTrack").append(`<img class="${this.cssClass}" id="${this.id}" name="${this.name}" src="${this.image}" width="${this.width}" height="${this.height}">`);
       this.makeCar = function(){return false;};
   };
+  this.generateListener = function(){
 
+  }
+
+}
+
+function countDown(countStart=5,displayTime=false,startRed=true,interval=100,endDate=new Date().getTime()){
+  let removeClass = "";
+  let addClass = "";
+  //determine if stoplight should start as red or green
+  if (startRed){
+    removeClass = "start";
+    addClass = "stop";
+  } else {
+    removeClass = "stop";
+    addClass = "start";
+  }
+
+  //add/remove appropriate classes for stoplight to display correctly as countdown begins
+  $("#stopLight").removeClass(removeClass).addClass(addClass);
+
+  //countdown
+  let intervalId = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = endDate - now;
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //if we are displaying the countdown set the html of the countDown element to num seconds left
+    if (displayTime){
+      $("#countDown").html(seconds);
+    }
+    //if countdown has elaspsed
+    if (distance <= 0){
+      //stop interval
+      clearInterval(intervalId);
+      //switch stoplight
+      $("#stopLight").removeClass(addClass).addClass(removeClass);
+    }
+  }, interval);
 }
