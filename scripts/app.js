46e7df12314fad6ddd6cc2e34e0e2e6e80f2076b
CalJersey <crippe@gmail.com>
2017-07-12 11:03:11 -0700

    fixing modals layout, cleaning up code , addig comments, removed start button (race start triggered by last user form)

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,27 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+
   let keyCode = 76;
   let car1 = new Car("car1","car1","car1","images/police.gif",0,0,0,116,65);
   car1.makeCar();
   let car2 = new Car("car2","car2","car2","images/police.gif",0,0,0,116,65);
   car2.makeCar();
 
-  let carChoices = [];
-  carChoices[0] = {'name':'police','image':'police.gif', 'width':288};
-  carChoices[1] = {'name':'brown','image':'brownCar.gif', 'width':288};
-  carChoices[2] = {'name':'tractor','image':'tractor.gif', 'width':288};
-  carChoices[3] = {'name':'bug','image':'bug.gif', 'width':288};
+  //global array of car objects
+  carChoices = [];
+  carChoices[0] = {'name':'police','image':'police.gif', 'width':288, 'thumbnailWidth':72,};
+  carChoices[1] = {'name':'brown','image':'brownCar.gif', 'width':288, 'thumbnailWidth':72,};
+  carChoices[2] = {'name':'tractor','image':'tractor.gif', 'width':288, 'thumbnailWidth':72,};
+  carChoices[3] = {'name':'bug','image':'bug.gif', 'width':288, 'thumbnailWidth':72,};
+
+  //setup car choice radio buttons and HTML sturctures to add to the form down below
+  carChoices.forEach(function(v,i){
+    let tempString = "";
+    tempString = `<span class="carForm"><img name="${v.name}" title="${v.name}" src="images/${v.image}" width="${v.thumbnailWidth}">`;
+    tempString = `${tempString}<br><input type="radio" name="carChoice" value="${i}"></span>`;
+    radioButContentStr = `${radioButContentStr}${tempString}`;
+  });
 
   //init Game
   game = new Game();
@@ -23,14 +33,14 @@ $(document).ready(function(){
 
   //2 Player Game listener
   $("#players2").on("click",function(){
-    game.players[0].playerForm.loadPlayerForm();
+    game.players[0].playerForm.loadPlayerForm(game.numPlayers-1);
     toggleButtons(1);
   });
   //3 Player Game listener
   $("#players3").on("click",function(){
     player = new Player();
     game.addPlayer(player);
-    game.players[0].playerForm.loadPlayerForm();
+    game.players[0].playerForm.loadPlayerForm(game.numPlayers-1);
     toggleButtons(1);
   });
   //4 Player Game listener
@@ -39,7 +49,7 @@ $(document).ready(function(){
     player1 = new Player();
     game.addPlayer(player0);
     game.addPlayer(player1);
-    game.players[0].playerForm.loadPlayerForm();
+    game.players[0].playerForm.loadPlayerForm(game.numPlayers-1);
     toggleButtons(1);
   });
   //startButton listener
@@ -55,6 +65,7 @@ $(document).ready(function(){
     event.preventDefault();
     if (event.which == keyCode){
       car1.incrementPosRight();
+      car2.incrementPosRight();
     };
 
   });
@@ -69,6 +80,7 @@ function Game(numPlayers=2,players=[],isStarted=0,isInProgress=0,isEnded=0,elaps
   this.elapsedTime = elapsedTime;
   this.addPlayer = function(player=[]){
     this.players.push(player);
+    this.numPlayers += 1;
   };
 }
 
@@ -85,10 +97,10 @@ function Car(id,name,cssClass,image="images/police.gif", speed=0, posRight=0, po
   this.actionKey = actionKey;
   this.setRight = function(posRight=this.posRight){$(`#${this.id}`).css({right:posRight})};
   //this.setTop = function(posY=posY){};
-  this.increaseSpeed = function(units=1){
+  this.increaseSpeed = function(units=10){
     this.speed += units;
   };
-  this.incrementPosRight = function(increment=1){
+  this.incrementPosRight = function(increment=10){
     this.posRight+=increment;
     this.setRight();
   }
@@ -109,6 +121,7 @@ function Player(name="",car=new Car(),index=0,actionKey=0,){
   this.name = name;
   this.car = car;
   this.index = index;
+  this.num = index+1;
   this.actionKey = actionKey;
   this.playerForm = new PlayerForm(this);
   this.setCar = function(car){
@@ -120,27 +133,56 @@ function Player(name="",car=new Car(),index=0,actionKey=0,){
   this.setName = function(name){
     this.name = name;
   }
-}
+  this.setPlayerForm = function(){
 
+  }
+}
+//Playerform object creates the modal window and option entry form for each player
 function PlayerForm(parent){
+  let radioButContentStr = "";
   this.parent = parent;
   this.playerIndex = this.parent.index;
+  this.playerNum = this.parent.num;
   this.playerName = this.parent.name;
   this.playerCar = this.parent.car;
   this.playerActionKey = this.parent.actionKey;
-  this.loadPlayerForm = function(playerIndex=this.playerIndex){
-    $("#modalContainer").append(`
-      <div class="easy-modal-animated" id="modalform${playerIndex}">
-        <div class="modalLabel">Driver ${playerIndex}</div>
-        <div class="modalContent>
-          Driver ${playerIndex}, please make your selections.
-        </div>
-        <form name="Driver${playerIndex}" action="#" method="POST">
-          <label for="Name">Name:</label>
-          <input type="text" name="DriverName" value="" class="driverName"/>
-          <label for="AcceleratorKey">Accelerator Key:</label>
-          <input type="text" name="Accelerator" value="" class="accelerator"/>
+  this.buttonStr = "";
 
+  //create form button for modal window - will either lead to next player or start race
+  this.createButtonStr = function(maxPlayerIndex){
+    let nextPlayer = 0;
+    if (this.playerIndex < maxPlayerIndex){
+        nextPlayer = this.playerIndex + 2; //index is one lower than player number so have to add two
+        this.buttonStr = `<a href="#" class="gameButton" id="playerFormSubmit">Proceed to Driver ${nextPlayer}</a>`;
+      } else {
+        this.buttonStr = `<a href="#" class="gameButton" id="startButton">Let's Race!</ra>`;
+      }
+  }
+  //return button content from creator function set ti tit to this instances
+  //this.buttonStr = this.createFormButtom(maxPlayerIndex);
+  //modal form to collect Player info
+  this.loadPlayerForm = function(maxPlayerIndex){
+    //create submit button with proper text and event trigger
+    this.createButtonStr(maxPlayerIndex);
+
+    //build the form and bind it to the modal
+    $("#modalContainer").append(`
+      <div class="easy-modal-animated" id="modalform${this.playerIndex}">
+        <div class="modalTitle">Driver ${this.playerNum}</div>
+        <div class="modalContent">
+          Driver ${this.playerNum}, please make your selections.
+        </div>
+        <form name="Driver${this.playerIndex}" action="#" method="POST">
+          <div class="modalForm">
+            <label class="modalLabel" for="Name">Name:</label>
+            <input type="text" name="DriverName" value="" size="15" class="driverName"/>
+          </div>
+          <div class="modalForm">
+            <label class="modalLabel" for="AcceleratorKey">Accelerator Key:</label>
+            <input type="text" name="Accelerator" value="" size="3" maxlength="1" class="accelerator"/>
+            <div class="imageCheckBoxes" id="imageCheckBoxes${this.playerIndex}"></div>
+            ${this.buttonStr}
+          </div>
         </form>
       </div>
     `
@@ -154,6 +196,13 @@ function PlayerForm(parent){
       transitionIn: 'animated fadeInUpBig',
       transitionOut: 'animated fadeOutUpBig',
       closeButtonClass: '.noClass'
+    });
+
+
+    //startButton listener - have to create it after the sturucture it is tied to is created
+    $("#playerFormSubmit").on("click", function(event){
+      event.preventDefault();
+      game.players[this.playerIndex+1].playerForm.loadPlayerForm(game.numPlayers-1);
     });
   }
 }
